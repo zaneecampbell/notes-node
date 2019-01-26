f018dfcc0256c66412237df50c2b113ce73c4f11
Zane Campbell <zanegitinit@gmail.com>
2019-01-26 13:44:32 -0800

    cleanup for user input

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-// console.log(_.isString(true));
-// console.log(_.isString("Zane"));
-let filteredArray = _.uniq(['Zane', 1, 'Zane', 1, 2, 3, 4])
-console.log(filteredArray);
-
-// let res = notes.add(9, -2);
-// console.log(res);
-
-// let user = os.userInfo();
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function (err) {
-//     if (err) {
-//         console.log('Unable to write to file');
-//     }
-// });
